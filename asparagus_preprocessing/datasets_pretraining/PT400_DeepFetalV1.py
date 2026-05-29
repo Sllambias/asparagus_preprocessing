9154536823113f715218fe0695f23651484c8f98
LlambiasMBP <llambias@live.com>
2026-05-29 19:54:31 +0200

    add DF PT

---
+++ asparagus_preprocessing/datasets_pretraining/PT400_DeepFetalV1.py
@@ -0,0 +1,48 @@
+import polars as pl
+from asparagus_preprocessing.utils.dataset_registration import register_dataset
+from asparagus_preprocessing.utils.parser import asparagus_parser
+from asparagus_preprocessing.utils.splitting import dynamic_split
+
+
+def main(
+    path: str = "/projects/users/people/seblla/repos/EHR_extract/all_images_2026-05-29.csv",
+    subdir: str = "",
+    processes=12,
+    bidsify=False,
+    save_dset_metadata=False,
+    save_as_tensor=False,
+):
+    df = pl.read_csv(path)
+    subjects = df["phair_hash"]
+
+    train_subjects, val_subjects, test_subjects = dynamic_split(
+        files=subjects, train_ratio=0.9, val_ratio=0.01, test_ratio=0.09
+    )
+    train_df = df.filter(pl.col("phair_hash").is_in(train_subjects))
+    train_files = train_df["no_ocr_preprocessed_file_path"].to_list()
+
+    val_df = df.filter(pl.col("phair_hash").is_in(val_subjects))
+    val_files = val_df["no_ocr_preprocessed_file_path"].to_list()
+
+    test_df = df.filter(pl.col("phair_hash").is_in(test_subjects))
+    test_files = test_df["no_ocr_preprocessed_file_path"].to_list()
+
+    register_dataset(
+        task_name="PT400_DeepFetalV1",
+        n_classes=1,
+        n_modalities=1,
+        train=train_files,
+        val=val_files,
+        test=test_files,
+        n_folds=1,
+    )
+
+
+if __name__ == "__main__":
+    args = asparagus_parser.parse_args()
+    main(
+        processes=args.num_workers,
+        bidsify=args.bidsify,
+        save_dset_metadata=args.save_dset_metadata,
+        save_as_tensor=args.save_as_tensor,
+    )
